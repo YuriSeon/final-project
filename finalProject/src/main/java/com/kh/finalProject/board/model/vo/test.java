5937b78388c48bf25950ec26bcb5862444642c3b
YuriSeon <129874234+YuriSeon@users.noreply.github.com>
2023-06-02 11:18:43 +0900

    test push

--- finalProject/src/main/java/com/kh/finalProject/board/model/vo/test.java
+++ finalProject/src/main/java/com/kh/finalProject/board/model/vo/test.java
@@ -4,14 +4,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class test {
+public class test { 
+	// 필요한 기능 테스트 해보려고 만든 클래스 
+	// java application으로 실행해서 테스트 해볼거라 main 넣음
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		WebDriver wb = new ChromeDriver();
 		
 		try {
-			wb.get("https://www.naver.com");
+			wb.get("http://www.naver.com");
+			System.out.println("실행");
+			
 		} finally {
 			wb.quit();
 		}
